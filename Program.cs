bc57710ce94031fd6cbb28d69eef54838a67b6dd
Michal Dziobek <michal.dziobek@comarch.pl>
2019-01-22 08:08:44 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace silver_octo_guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
